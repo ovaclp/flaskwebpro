f345ef1665a0f54bef052c57ae0044b1a4c41d22
ovaclp <ova_clp@163.com>
2020-07-31 16:16:18 +0800

    3-1

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask_script import Manager
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, request, make_response, redirect, abort, render_template
 
 
 app = Flask(__name__)
@@ -7,19 +7,12 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    return redirect('http://www.baidu.com')
+    return render_template('index.html')
 
-
-# @app.route('/user/<id>')
-# def get_user(id):
-#     user = load_user(id)
-#     if not user:
-#         abort(404)
-#     return '<h1>Hello, %s</h1>' % user.name
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 
 if __name__=='__main__':
